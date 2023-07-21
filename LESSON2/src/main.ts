2f53df468c8aa78c03e7581d9c6be6c973d7689c
VEN CHESTER <veniann01@gmail.com>
2023-07-21 22:36:36 +0800

    Lesson2 is for Data types for typescript

---
+++ LESSON2/src/main.ts
@@ -0,0 +1,23 @@
+let myName: string = "ven"
+let meaningOfLife: number;
+let isLoading: boolean;
+let album:any
+// let album: string | number  //UNION
+
+
+myName = "chester"
+meaningOfLife = 42
+isLoading = true
+album = 4141
+
+
+const sum = (a: number,b: string) => {
+    return a + b
+}
+
+
+let postId: string | number;
+let isActive: number | boolean 
+
+
+let re:RegExp = /\w+/g
